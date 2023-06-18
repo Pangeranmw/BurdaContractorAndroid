2b1ac45f91d5695cb21f81b99f505f86575c2d7c
pangeranmw <pangeranwaliyullah@gmail.com>
2023-06-19 00:48:19 +0800

    update class diagram and repo

--- app/src/main/java/com/android/burdacontractor/feature/beranda/presentation/BerandaFragment.kt
+++ app/src/main/java/com/android/burdacontractor/feature/beranda/presentation/BerandaFragment.kt
@@ -2,33 +2,37 @@ package com.android.burdacontractor.feature.beranda.presentation
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.android.burdacontractor.R
 import com.android.burdacontractor.core.domain.model.enums.StateResponse
+import com.android.burdacontractor.core.presentation.adapter.ListSuratJalanAdapter
+import com.android.burdacontractor.core.service.location.LocationService
+import com.android.burdacontractor.core.utils.checkConnection
 import com.android.burdacontractor.core.utils.openActivity
 import com.android.burdacontractor.core.utils.setGone
 import com.android.burdacontractor.core.utils.setVisible
 import com.android.burdacontractor.databinding.FragmentBerandaBinding
 import com.android.burdacontractor.feature.auth.presentation.AuthViewModel
 import com.android.burdacontractor.feature.auth.presentation.LoginActivity
-import com.android.burdacontractor.core.service.location.LocationService
-import com.android.burdacontractor.core.presentation.StorageViewModel
+import com.android.burdacontractor.feature.suratjalan.presentation.SuratJalanFragment
+import com.android.burdacontractor.feature.suratjalan.presentation.SuratJalanViewModel
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class BerandaFragment : Fragment() {
     private val authViewModel: AuthViewModel by activityViewModels()
-    private val storageViewModel: StorageViewModel by viewModels()
     private var _binding: FragmentBerandaBinding? = null
     private val binding get() = _binding!!
     private var snackbar: Snackbar? = null
+    private val suratJalanViewModel: SuratJalanViewModel by viewModels()
+    private lateinit var adapter: ListSuratJalanAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -42,7 +46,7 @@ class BerandaFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         authViewModel.liveNetworkChecker.observe(viewLifecycleOwner){
-            checkConnection(it)
+            requireContext().checkConnection(it, binding.root){ initObserver() }
         }
     }
 
@@ -52,6 +56,7 @@ class BerandaFragment : Fragment() {
                 StateResponse.LOADING -> binding.progressBar.setVisible()
                 StateResponse.ERROR -> binding.progressBar.setGone()
                 StateResponse.SUCCESS -> {
+                    // Logout
                     binding.progressBar.setGone()
                     Intent(requireActivity(), LocationService::class.java).apply {
                         action = LocationService.ACTION_STOP
@@ -71,23 +76,30 @@ class BerandaFragment : Fragment() {
                 ).show()
             }
         }
+        initUi()
     }
     private fun initUi(){
         binding.btnLogout.setOnClickListener {
             authViewModel.logout()
         }
     }
-    private fun checkConnection(state: Boolean){
-        if(!state){
-            snackbar = Snackbar.make(binding.root, R.string.no_internet, Snackbar.LENGTH_INDEFINITE)
-            snackbar?.setBackgroundTint(ContextCompat.getColor(requireContext(),R.color.error_color))
-            snackbar?.show()
-        }else{
-            snackbar?.setText(R.string.connected)
-            snackbar?.setBackgroundTint(ContextCompat.getColor(requireContext(),R.color.green_light_full))
-            snackbar?.dismiss()
-            initObserver()
-            initUi()
+    private fun setAdapter(search: String? = null){
+//        if(adapter!=null) adapter.refresh()
+        adapter = ListSuratJalanAdapter{
+            val mBundle = Bundle()
+            mBundle.putString(SuratJalanFragment.SURAT_JALAN_ID, it.id)
+            findNavController().navigate(R.id.action_surat_jalan_fragment_to_detailSuratJalanFragment, mBundle)
         }
+        binding.rvSuratJalan.adapter = adapter
+//        suratJalanViewModel.getAllSuratJalan(
+//            tipe,
+//            status,
+//            dateStart,
+//            dateEnd,
+//            10,
+//            search,
+//        ).observe(viewLifecycleOwner) {
+//            adapter.submitData(lifecycle, it)
+//        }
     }
 }
